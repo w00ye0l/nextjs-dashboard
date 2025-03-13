6f044f5d1d2ef880a295b36ea2a18289b4e5bd8e
lwyeol <lwyeol1010@azero.biz>
2025-03-13 09:55:46 +0900

    fix: next lint build error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
+    console.error(error);
     return {
       message: "Database Error: Failed to Create Invoice",
     };
@@ -100,6 +101,7 @@ export async function updateInvoice(
     WHERE id = ${id}
   `;
   } catch (error) {
+    console.error(error);
     return { message: "Database Error: Failed to Update Invoice" };
   }
 
